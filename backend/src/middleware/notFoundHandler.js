3ce3647bf3b221592abc96a4601440863b478abb
wassimtajeddin <wassim.tajeddin@gmail.com>
2025-12-07 15:00:21 +0100

    Add notFoundHndler

---
+++ backend/src/middleware/notFoundHandler.js
@@ -0,0 +1,8 @@
+const notFoundHandler = (req, res, next) => {
+  const error = new Error(`Route not found: ${req.method} ${req.originalUrl}`)
+  error.status = 404
+  
+  next(error)
+}
+
+module.exports = notFoundHandler
